2cfbd977c3b1ab990f93e3314b2c58f1f321c061
Mojoryson <rod.morrison@gmail.com>
2025-01-06 15:26:00 -0600

    Add S&P 500 Streamlit app with data retrieval and sector filtering

---
+++ sp_500/sp_app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import base64
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+st.title("S&P 500 App")
+
+st.markdown("""
+        This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+        * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+        * **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+        """)
+
+st.sidebar.header("User Inputs")
+
+# load the S&P 500 data from the web
+@st.cache_data
+def load_data():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    return df
+
+df = load_data()
+sector = df.groupby("GICS Sector")
+
+# Build out the sidebar with the sector selection
+sorted_sector_unique = sorted(df["GICS Sector"].unique())
+selected_sector = st.sidebar.multiselect("Sector", sorted_sector_unique, sorted_sector_unique)
+
+# Filter the data
+df_selected_sector = df[(df["GICS Sector"].isin(selected_sector))]
+
+# Display the selected sector data in the UI
+st.header("Display Companies in Selected Sector")
+st.write("Data Dimension: " + str(df_selected_sector.shape[0]) + " rows and " + str(df_selected_sector.shape[1]) + " columns.")
+st.dataframe(df_selected_sector)
+
+# Download S&P 500 data
+def file_download(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    return href
+
+st.markdown(file_download(df_selected_sector), unsafe_allow_html=True)
+
